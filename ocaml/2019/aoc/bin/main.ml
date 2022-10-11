9149ff5d71774e9e43aea2c57620c2f1feaea310
Yannis Katsaros <yannis@katsaros.io>
2022-10-11 08:49:30 -0500

    Day 2 part I intcode

--- ocaml/2019/aoc/bin/main.ml
+++ ocaml/2019/aoc/bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+open Aoc
+
+module Day2 = struct
+
+  let init memory =
+    memory.(1) <- 12 ;
+    memory.(2) <- 2
+
+  let part1 memory =
+    init memory ;
+    let memory' = Intcode.run memory in
+    Format.printf "%d\n" memory'.(0)
+
+end
+
+let () =
+  let memory = Intcode.array_of_file Sys.argv.(1) in
+  Day2.part1 memory
