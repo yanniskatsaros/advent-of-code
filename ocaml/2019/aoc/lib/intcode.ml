e66af7734a61e4e1930fcc8a3a70cdf1b4474497
Yannis Katsaros <yannis@katsaros.io>
2022-10-11 08:43:50 -0500

    Add memory layout and instruction parsing for Day 2 part I

--- ocaml/2019/aoc/lib/intcode.ml
+++ ocaml/2019/aoc/lib/intcode.ml
@@ -4,3 +4,50 @@ let array_of_file file =
   lines
     |> List.map (fun s -> Int.of_float (Float.of_string s))
     |> Array.of_list
+
+type error =
+  | InvalidOpcode of int
+  | MemoryOutOfBounds of int
+
+type state =
+  | Pos of int
+  | Halt
+
+let op memory pos =
+  try match memory.(pos) with
+  | 1 -> begin
+    let pos1 = memory.(pos + 1) in
+    let pos2 = memory.(pos + 2) in
+    let target = memory.(pos + 3) in
+    let a = memory.(pos1) in
+    let b = memory.(pos2) in
+    memory.(target) <- a + b ;
+    Ok (Pos (pos + 4))
+  end
+  | 2 -> begin
+    let pos1 = memory.(pos + 1) in
+    let pos2 = memory.(pos + 2) in
+    let target = memory.(pos + 3) in
+    let a = memory.(pos1) in
+    let b = memory.(pos2) in
+    memory.(target) <- a * b ;
+    Ok (Pos (pos + 4))
+  end
+  | 99 -> Ok Halt
+  | opcode -> Error (InvalidOpcode opcode)
+  with Invalid_argument _ -> Error (MemoryOutOfBounds pos)
+
+
+let run memory =
+  let rec go memory pos =
+    match op memory pos with
+    | Ok (Pos pos') -> go memory pos'
+    | Ok Halt -> memory
+    | Error (MemoryOutOfBounds i )->
+      Format.printf "memory out of bounds: %d" i;
+      memory
+    | Error (InvalidOpcode code) ->
+      Format.printf "invalid opcode: %d" code ;
+      memory
+  in
+  go memory 0
