0409f62dee27bcab25ee786890d4cad908310038
Yannis Katsaros <yannis@katsaros.io>
2022-10-18 16:24:51 -0500

    Big cleanup after refactoring

--- ocaml/2019/aoc/bin/day2.ml
+++ ocaml/2019/aoc/bin/day2.ml
@@ -21,6 +21,34 @@ let part1 () =
   | Error s ->
     Printf.printf "%s\n" s
 
+exception Done of (int * int)
+
+let part2 () =
+  let open Memory in
+  let open Cpu in
+
+  (* loop through all 100x100 combinations,
+     initializing fresh memory each time *)
+  try
+    for i = 1 to 100 do
+      for j = 1 to 100 do
+        let memory = Memory.copy mem in
+        memory.$(1) <- i;
+        memory.$(2) <- j;
+        let cpu = (Cpu.init memory) in
+
+        match run cpu with
+        | Ok () ->
+          if cpu.mem.$(0) = 19690720
+            then raise (Done (i, j))
+            else ()
+        | Error _ -> ()
+      done
+    done;
+  with Done (x, y) ->
+    Printf.printf "%d\n" (100 * x + y)
+
 
 let () =
-  part1 ()
+  part1 ();
+  part2 ()
--- ocaml/2019/aoc/bin/main.ml
+++ ocaml/2019/aoc/bin/main.ml
@@ -1,18 +1,30 @@
-open Aoc
+(*
+  Terms:
+  - memory: the Intcode program represented as a list of integers
+  - address: a position in memory
+  - instruction: an "opcode" such as 1, 2, 99 etc.
+  - instruction parameters:
+    the values immediately after an opcode. for example, in the
+    instruction [1, 2, 3, 4] 1 is the opcode, and
+    2, 3, 4 are the instruction parameters. 99 contains just an
+    opcode and *no* parameters.
+  - instruction pointer:
+    the address of the current instruction. this is initially 0.
+    after an instruction finishes, the instruction pointer increases
+    by the number of values in the instruction. for example, for
+    the instruction [1, 2, 3, 4] => 4 (1 opcode + 3 parameters)
 
-module Day2 = struct
-
-  let init memory =
-    memory.(1) <- 12 ;
-    memory.(2) <- 2
-
-  let part1 memory =
-    init memory ;
-    let memory' = Intcode.run memory in
-    Format.printf "%d\n" memory'.(0)
-
-end
-
-let () =
-  let memory = Intcode.array_of_file Sys.argv.(1) in
-  Day2.part1 memory
+  General approach:
+  1. initialize memory (in some way, typically setting the value at address 1 and 2?)
+  2. initialize address pointer to 0  
+  3. parse instruction (fallible)
+    - add
+    - multiply
+    - other?
+    - halt => Ok ()
+    - error => Error
+  4. perform operation
+    - mutate memory (fallible)
+    - update instruction pointer
+  5. repeat
+ *)
