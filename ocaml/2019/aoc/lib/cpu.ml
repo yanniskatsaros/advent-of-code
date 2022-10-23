f7ce6974e51573362a0c335063b330f44c10a019
Yannis Katsaros <yannis@katsaros.io>
2022-10-23 16:56:52 -0500

    Add Cpu.of_string and Cpu.equal

--- ocaml/2019/aoc/lib/cpu.ml
+++ ocaml/2019/aoc/lib/cpu.ml
@@ -7,3 +7,13 @@ type t =
 
 let init mem io =
   { ip = 0; mem; io }
+
+let of_string str =
+  let memory = Memory.of_string str in
+  let io = Io.init () in
+  init memory io
+
+let equal c1 c2 =
+  ( c1.mem = c2.mem )
+    && ( c1.io = c2.io )
+    && ( c1.ip = c2.ip )
