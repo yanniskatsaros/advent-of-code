bb1b6e335d8834d9574e50d51d4efb3860bbfbe3
Yannis Katsaros <yannis@katsaros.io>
2022-10-23 13:05:18 -0500

    Add Day 5 part I solution

---
+++ ocaml/2019/aoc/bin/day5.ml
@@ -0,0 +1,16 @@
+open Aoc
+
+let mem = Memory.of_file Sys.argv.(1)
+
+let part1 () =
+  let memory = Memory.copy mem in
+  let io = Io.init () in
+  Io.write_stdin io 1 ;
+  let cpu = (Cpu.init memory io) in
+
+  match Instruction.run cpu with
+    | Ok () -> Printf.printf "%d\n" (Io.read_stdout io)
+    | Error s -> Printf.printf "%s\n" s
+
+let () =
+  part1 ()
