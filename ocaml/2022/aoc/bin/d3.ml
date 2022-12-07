85df0209f34e378e9e8d8643fa6630907842b76c
Yannis Katsaros <yannis@katsaros.io>
2022-12-06 21:01:19 -0600

    Add day 3 part II solution

--- ocaml/2022/aoc/bin/d3.ml
+++ ocaml/2022/aoc/bin/d3.ml
@@ -26,6 +26,22 @@ let common_char (a, b) =
     | None -> None
     | Some (c, _) -> Some c
 
+(** returns the first common [char] between the three strings, if one exists *)
+let common_char2 (a, b, c) =
+  let set_a = CharSet.of_seq (String.to_seq a) in
+  let set_b = CharSet.of_seq (String.to_seq b) in
+  let set_c = CharSet.of_seq (String.to_seq c) in
+
+  (* the set intersection of A and B *)
+  let ab = CharSet.inter set_a set_b in
+  (* intersected with C *)
+  let abc = CharSet.inter ab set_c in
+
+  (* but we only expect a single common item *)
+  match Seq.uncons (CharSet.to_seq abc) with
+    | None -> None
+    | Some (c, _) -> Some c
+
 let priority c = match c with
   | 'a' .. 'z' -> (* priority 1 - 26 *)
     (* lowercase a has ASCII code 97 *)
@@ -41,6 +57,18 @@ let parse str =
     | None -> 0
     | Some c -> priority c
 
+let parse_triple lst =
+  let arr = Array.of_list lst in
+
+  try
+    let s1 = arr.(0) in
+    let s2 = arr.(1) in
+    let s3 = arr.(2) in
+    match common_char2 (s1, s2, s3) with
+      | None -> 0
+      | Some c -> priority c
+  with _ -> 0
+
 let part1 str =
   str
     |> String.trim
@@ -48,9 +76,20 @@ let part1 str =
     |> List.map parse
     |> sum
 
+let part2 str =
+  str
+    |> String.trim
+    |> String.split_on_char '\n'
+    |> List.to_seq
+    |> chunks 3
+    |> List.map parse_triple
+    |> sum
 
 let () =
   let input = read_file Sys.argv.(1) in
 
   let p1 = part1 input in
-  Format.printf "Part 1: %d\n" p1
+  Format.printf "Part 1: %d\n" p1;
+
+  let p2 = part2 input in
+  Format.printf "Part 2: %d\n" p2
