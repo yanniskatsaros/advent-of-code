0c7634efdab6881bbc961b305bacfe034fdb83db
Yannis Katsaros <yannis@katsaros.io>
2022-12-10 16:16:57 -0600

    Add day 6 part I solution

--- rust/2022/src/bin/d6.rs
+++ rust/2022/src/bin/d6.rs
@@ -1,12 +1,38 @@
+use std::collections::hash_map::RandomState;
+use std::collections::HashSet;
 use std::env;
 use std::error::Error;
 use std::fs::read_to_string;
+
+fn part1(s: &str) {
+    let chars = s.chars().collect::<Vec<_>>();
+    let windows = chars.windows(4);
+    let markers: Vec<_> = windows
+        .enumerate()
+        .filter_map(|(i, w)| {
+            let set: HashSet<_, RandomState> = HashSet::from_iter(w);
+            // add 4 since enumerating begins counting the first window at 0
+            let j = i + 4;
+
+            if set.len() == 4 {
+                Some(j)
+            } else {
+                None
+            }
+        })
+        .take(1)
+        .collect();
+
+    let i = markers[0];
+    println!("Part I: {i}");
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
     let input = read_to_string(path)?.trim().to_string();
 
-    dbg!(input);
+    part1(&input);
+
     Ok(())
 }
