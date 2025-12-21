d926ce3be6d32c0e265ab14fb77abc3b8a8e779e
Yannis Katsaros <yannis@fridayharbor.ai>
2025-12-21 16:35:25 -0600

    Day 4 part I solution

--- go/2025/day/d04/d4.go
+++ go/2025/day/d04/d4.go
@@ -2,7 +2,10 @@ package d04
 
 import (
 	"fmt"
+	"iter"
 	"strings"
+
+	"yanniskatsaros/aoc/2025/utils"
 )
 
 type Coord struct {
@@ -17,13 +20,52 @@ const (
 	Roll
 )
 
-func ParseGridDiagram(diagram string) (map[Coord]GridItem, error) {
+type Grid struct {
+	grid map[Coord]GridItem
+	rows int
+	cols int
+}
+
+func EmptyGrid() Grid {
+	grid := make(map[Coord]GridItem)
+	return Grid{grid: grid, rows: 0, cols: 0}
+}
+
+func (g Grid) Items() iter.Seq2[Coord, GridItem] {
+	return func(yield func(Coord, GridItem) bool) {
+		// iterate this way to retain (x, y) grid ordering
+		for i := range g.rows {
+			for j := range g.cols {
+				coord := Coord{x: i, y: j}
+				item := g.grid[coord]
+
+				if !yield(coord, item) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (g Grid) WithinBounds(coord Coord) bool {
+	return (coord.x >= 0) &&
+		(coord.x < g.rows) &&
+		(coord.y >= 0) &&
+		(coord.y < g.cols)
+}
+
+func ParseGridDiagram(diagram string) (Grid, error) {
 	grid := make(map[Coord]GridItem)
 
 	lines := strings.Split(strings.TrimSpace(diagram), "\n")
 
+	// track the total number of rows and columns in the grid
+	rows := len(lines)
+	cols := 0
+
 	for i, line := range lines {
 		line = strings.TrimSpace(line)
+		cols = len(line)
 
 		for j, c := range line {
 			var item GridItem
@@ -36,7 +78,7 @@ func ParseGridDiagram(diagram string) (map[Coord]GridItem, error) {
 				item = Empty
 
 			default:
-				return nil, fmt.Errorf("Found unknown character when parsing grid: %v", string(c))
+				return EmptyGrid(), fmt.Errorf("Found unknown character when parsing grid: %v", string(c))
 			}
 
 			coord := Coord{x: i, y: j}
@@ -46,28 +88,77 @@ func ParseGridDiagram(diagram string) (map[Coord]GridItem, error) {
 		}
 	}
 
-	return grid, nil
+	return Grid{grid: grid, rows: rows, cols: cols}, nil
+}
+
+func CountRolls(items map[Coord]GridItem) int {
+	count := 0
+
+	for _, item := range items {
+		if item == Roll {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (g Grid) AdjacentItems(coord Coord) map[Coord]GridItem {
+	adj := make(map[Coord]GridItem)
+
+	// We need to check 8 total positions surrounding the given `coord`
+	//
+	// 123
+	// 8@4
+	// 765
+	//
+	// NOTE: we assume **top left** is the (0, 0) coordinate position
+
+	relpos := []int{-1, 0, 1}
+
+	for _, i := range relpos {
+		for _, j := range relpos {
+			if (i == 0) && (j == 0) {
+				// the current index of `coord` is not considered adjacent
+				continue
+			}
+
+			c := Coord{x: coord.x + i, y: coord.y + j}
+			if g.WithinBounds(c) {
+				adj[c] = g.grid[c]
+			}
+		}
+	}
+
+	return adj
 }
 
 func Part1() {
-	diagram := `
-..@@.@@@@.
-@@@.@.@.@@
-@@@@@.@.@@
-@.@@@@..@.
-@@.@@@@.@@
-.@@@@@@@.@
-.@.@.@.@@@
-@.@@@.@@@@
-.@@@@@@@@.
-@.@.@@@.@.
-	`
+	diagram, err := utils.ReadInput("inputs/day4.txt")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
 
+	diagram = strings.TrimSpace(diagram)
 	grid, err := ParseGridDiagram(diagram)
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
 
-	fmt.Printf("grid = %v", grid)
+	// fmt.Printf("grid = %v", grid)
+
+	total := 0
+	for coord, item := range grid.Items() {
+		adj := grid.AdjacentItems(coord)
+		count := CountRolls(adj)
+
+		if (item == Roll) && (count < 4) {
+			total++
+			// fmt.Printf("coord = %v\n", coord)
+		}
+	}
+
+	fmt.Printf("Total = %v\n", total)
 }
--- go/2025/day/d04/d4_test.go
+++ go/2025/day/d04/d4_test.go
@@ -9,20 +9,27 @@ import (
 func TestParseGridDiagram(t *testing.T) {
 	tests := []struct {
 		input   string
-		want    map[Coord]GridItem
+		want    Grid
 		wantErr bool
 	}{
-		{input: ".@.\n@@@\n...", want: map[Coord]GridItem{
-			Coord{x: 0, y: 0}: Empty,
-			Coord{x: 0, y: 1}: Roll,
-			Coord{x: 0, y: 2}: Empty,
-			Coord{x: 1, y: 0}: Roll,
-			Coord{x: 1, y: 1}: Roll,
-			Coord{x: 1, y: 2}: Roll,
-			Coord{x: 2, y: 0}: Empty,
-			Coord{x: 2, y: 1}: Empty,
-			Coord{x: 2, y: 2}: Empty,
-		}, wantErr: false},
+		{
+			input: ".@.\n@@@\n...", want: Grid{
+				grid: map[Coord]GridItem{
+					Coord{x: 0, y: 0}: Empty,
+					Coord{x: 0, y: 1}: Roll,
+					Coord{x: 0, y: 2}: Empty,
+					Coord{x: 1, y: 0}: Roll,
+					Coord{x: 1, y: 1}: Roll,
+					Coord{x: 1, y: 2}: Roll,
+					Coord{x: 2, y: 0}: Empty,
+					Coord{x: 2, y: 1}: Empty,
+					Coord{x: 2, y: 2}: Empty,
+				},
+				rows: 3,
+				cols: 3,
+			},
+			wantErr: false,
+		},
 	}
 
 	for i, tc := range tests {
@@ -37,6 +44,49 @@ func TestParseGridDiagram(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("input = %v, want = %v, got = %v", tc.input, tc.want, got)
+			}
+		})
+	}
+}
+
+func TestGridWithinBounds(t *testing.T) {
+	grid := Grid{
+		grid: map[Coord]GridItem{
+			Coord{x: 0, y: 0}: Empty,
+			Coord{x: 0, y: 1}: Roll,
+			Coord{x: 0, y: 2}: Empty,
+			Coord{x: 1, y: 0}: Roll,
+			Coord{x: 1, y: 1}: Roll,
+			Coord{x: 1, y: 2}: Roll,
+			Coord{x: 2, y: 0}: Empty,
+			Coord{x: 2, y: 1}: Empty,
+			Coord{x: 2, y: 2}: Empty,
+		},
+		rows: 3,
+		cols: 3,
+	}
+
+	tests := []struct {
+		input Coord
+		want  bool
+	}{
+		{input: Coord{x: 0, y: 0}, want: true},
+		{input: Coord{x: 0, y: 2}, want: true},
+		{input: Coord{x: 2, y: 2}, want: true},
+		{input: Coord{x: -1, y: 0}, want: false},
+		{input: Coord{x: -1, y: -1}, want: false},
+		{input: Coord{x: 0, y: -1}, want: false},
+		{input: Coord{x: 3, y: 0}, want: false},
+	}
+
+	for i, tc := range tests {
+		name := fmt.Sprintf("test %d", i)
+
+		t.Run(name, func(t *testing.T) {
+			got := grid.WithinBounds(tc.input)
+
+			if got != tc.want {
 				t.Errorf("input = %v, want = %v, got = %v", tc.input, tc.want, got)
 			}
 		})
